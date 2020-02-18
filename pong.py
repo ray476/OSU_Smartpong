bc62e157614b0c3b82ffa60e0a14ab8cf31f501f
Ray, Matthew J <ray.476@buckeyemail.osu.edu>
2020-02-18 18:05:01 -0500

    try, finally added main loop

--- pong.py
+++ pong.py
@@ -113,69 +113,75 @@ reward_sum = 0
 episode_number = 0
 if collection:
     data_collect = open(Interface.dcFilename(), 'w')
-while True:
-    if render: env.render()
+try:
+    while True:
+        if render: env.render()
 
-    # preprocess the observation, set input to network to be difference image
-    cur_x = prepro(observation)
-    x = cur_x - prev_x if prev_x is not None else np.zeros(D)
-    prev_x = cur_x
+        # preprocess the observation, set input to network to be difference image
+        cur_x = prepro(observation)
+        x = cur_x - prev_x if prev_x is not None else np.zeros(D)
+        prev_x = cur_x
 
-    # forward the policy network and sample an action from the returned probability
-    aprob, h = policy_forward(x)
-    action = 2 if np.random.uniform() < aprob else 3  # roll the dice!
+        # forward the policy network and sample an action from the returned probability
+        aprob, h = policy_forward(x)
+        action = 2 if np.random.uniform() < aprob else 3  # roll the dice!
 
-    # record various intermediates (needed later for backprop)
-    xs.append(x)  # observation
-    hs.append(h)  # hidden state
-    y = 1 if action == 2 else 0  # a "fake label"
-    dlogps.append(
-        y - aprob)  # grad that encourages the action that was taken to be taken (see http://cs231n.github.io/neural-networks-2/#losses if confused)
+        # record various intermediates (needed later for backprop)
+        xs.append(x)  # observation
+        hs.append(h)  # hidden state
+        y = 1 if action == 2 else 0  # a "fake label"
+        dlogps.append(
+            y - aprob)  # grad that encourages the action that was taken to be taken (see http://cs231n.github.io/neural-networks-2/#losses if confused)
 
-    # step the environment and get new measurements
-    observation, reward, done, info = env.step(action)
-    reward_sum += reward
+        # step the environment and get new measurements
+        observation, reward, done, info = env.step(action)
+        reward_sum += reward
 
-    drs.append(reward)  # record reward (has to be done after we call step() to get reward for previous action)
+        drs.append(reward)  # record reward (has to be done after we call step() to get reward for previous action)
 
-    if done:  # an episode finished
-        if collection: data_collect.write('{} {} '.format(episode_number, reward_sum))
-        episode_number += 1
+        if done:  # an episode finished
+            # data write
+            if collection: data_collect.write('{} {} '.format(episode_number, reward_sum))
+            episode_number += 1
 
-        # stack together all inputs, hidden states, action gradients, and rewards for this episode
-        epx = np.vstack(xs)
-        eph = np.vstack(hs)
-        epdlogp = np.vstack(dlogps)
-        epr = np.vstack(drs)
-        xs, hs, dlogps, drs = [], [], [], []  # reset array memory
+            # stack together all inputs, hidden states, action gradients, and rewards for this episode
+            epx = np.vstack(xs)
+            eph = np.vstack(hs)
+            epdlogp = np.vstack(dlogps)
+            epr = np.vstack(drs)
+            xs, hs, dlogps, drs = [], [], [], []  # reset array memory
 
-        # compute the discounted reward backwards through time
-        discounted_epr = discount_rewards(epr)
-        # standardize the rewards to be unit normal (helps control the gradient estimator variance)
-        discounted_epr -= np.mean(discounted_epr)
-        discounted_epr /= np.std(discounted_epr)
+            # compute the discounted reward backwards through time
+            discounted_epr = discount_rewards(epr)
+            # standardize the rewards to be unit normal (helps control the gradient estimator variance)
+            discounted_epr -= np.mean(discounted_epr)
+            discounted_epr /= np.std(discounted_epr)
 
-        epdlogp *= discounted_epr  # modulate the gradient with advantage (PG magic happens right here.)
-        grad = policy_backward(eph, epdlogp)
-        for k in model: grad_buffer[k] += grad[k]  # accumulate grad over batch
+            epdlogp *= discounted_epr  # modulate the gradient with advantage (PG magic happens right here.)
+            grad = policy_backward(eph, epdlogp)
+            for k in model: grad_buffer[k] += grad[k]  # accumulate grad over batch
 
-        # perform rmsprop parameter update every batch_size episodes
-        if episode_number % batch_size == 0:
-            for k, v in model.items():
-                g = grad_buffer[k]  # gradient
-                rmsprop_cache[k] = decay_rate * rmsprop_cache[k] + (1 - decay_rate) * g ** 2
-                model[k] += learning_rate * g / (np.sqrt(rmsprop_cache[k]) + 1e-5)
-                grad_buffer[k] = np.zeros_like(v)  # reset batch gradient buffer
+            # perform rmsprop parameter update every batch_size episodes
+            if episode_number % batch_size == 0:
+                for k, v in model.items():
+                    g = grad_buffer[k]  # gradient
+                    rmsprop_cache[k] = decay_rate * rmsprop_cache[k] + (1 - decay_rate) * g ** 2
+                    model[k] += learning_rate * g / (np.sqrt(rmsprop_cache[k]) + 1e-5)
+                    grad_buffer[k] = np.zeros_like(v)  # reset batch gradient buffer
 
-        # boring book-keeping
-        running_reward = reward_sum if running_reward is None else running_reward * 0.99 + reward_sum * 0.01
-        print('resetting env. episode reward total was %f. running mean: %f' % (reward_sum, running_reward))
-        if collection: data_collect.write('{}\n'.format(running_reward))
-        if episode_number % 100 == 0: pickle.dump(model, open(filename, 'wb'))
-        reward_sum = 0
-        observation = env.reset()  # reset env
-        prev_x = None
+            # boring book-keeping
+            running_reward = reward_sum if running_reward is None else running_reward * 0.99 + reward_sum * 0.01
+            print('resetting env. episode reward total was %f. running mean: %f' % (reward_sum, running_reward))
+            # data write pt 2
+            if collection: data_collect.write('{}\n'.format(running_reward))
+            if episode_number % 100 == 0: pickle.dump(model, open(filename, 'wb'))
+            reward_sum = 0
+            observation = env.reset()  # reset env
+            prev_x = None
 
-    if reward != 0:  # Pong has either +1 or -1 reward exactly when game ends.
-        print('ep %d: game finished, reward: %f' % (episode_number, reward), '' if reward == -1 else ' !!!!!!!!')
-
+        if reward != 0:  # Pong has either +1 or -1 reward exactly when game ends.
+            print('ep %d: game finished, reward: %f' % (episode_number, reward), '' if reward == -1 else ' !!!!!!!!')
+finally:
+    print('Closing files and pickling current model')
+    data_collect.close()
+    pickle.dump(model, open(filename, 'wb'))
