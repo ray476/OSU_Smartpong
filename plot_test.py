d0acac7fa6787e194d7972f875cb99493efe2d64
Ray, Matthew J <ray.476@buckeyemail.osu.edu>
2020-02-21 10:33:23 -0500

    plot text scales off of range, rather than hardcoded

--- plot_test.py
+++ plot_test.py
@@ -2,6 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+def string_offsets(ranges):
+    if ranges[0] < 0 and ranges[1] < 0:
+        return ranges[1] + (ranges[0] - ranges[1]) / 5
+    else:
+        return ranges[0] + (ranges[1] - ranges[0]) / 5
+
+
+
+
 def best_fit_linear(X, Y):
 
     xbar = sum(X)/len(X)
@@ -19,7 +28,7 @@ def best_fit_linear(X, Y):
     return a, b
 
 
-f = open('./first_model/new_results', 'r')
+f = open('./first_model/mod_results', 'r')
 array = np.loadtxt(f)
 
 a, b = best_fit_linear(array[:, 0], array[:, 1])
@@ -30,6 +39,9 @@ plt.plot(array[:, 0], yfit)
 plt.ylabel('Running Mean')
 plt.xlabel('Episodes')
 plt.title('Mean vs Episodes')
-plt.text(200, -14, 'best fit line:\ny = {:.2f} + {:.4f}x'.format(a, b))
+# get ranges to scale best fit location
+# hard coded for this data, will need to read from plot() return
+plt.text(string_offsets(plt.xlim()), string_offsets(plt.ylim()), 'best fit line:\ny = {:.2f} + {:.4f}x'.format(a, b))
+
 plt.show()
 
